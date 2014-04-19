62c391760d539323a6889ea7dc184d838dc22c79
Sharon <seaustin@live.unc.edu>
2014-04-18 20:19:56 -0400

    writing out the slider js

---
+++ js/slideshow.js
@@ -0,0 +1,11 @@
+// jQuery for the sliding features of WildCats
+
+$(document).ready(function() {
+
+	//get the number of slides we have
+	var slideNumber = $('.slide').length;
+
+	/* default setup for the page: one opening screen */
+
+
+} // close the script
