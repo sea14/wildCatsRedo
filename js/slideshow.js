ba2736c366ea6dfb9d375a96686b8dec82656a3b
Sharon <seaustin@live.unc.edu>
2014-04-18 20:29:46 -0400

    start of a function for the next button

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,9 +3,17 @@
 $(document).ready(function() {
 
 	//get the number of slides we have
-	var slideNumber = $('.slide').length;
+	var slideNumber = $('.slide > section').length;
 
-	/* default setup for the page: one opening screen */
+	/* default setup for the page: one opening screen
+	 * if the right arrow is clicked, we move to the next slide
+	 */
 
+	 	$(".nextButton").click(function(){
+
+	 		
+
+
+	 	});
 
 } // close the script
