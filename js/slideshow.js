3c7c9f6cd0f8bbd5b82f78bb22bb66dcfe806153
Sharon <seaustin@live.unc.edu>
2014-04-21 23:11:28 -0400

    fleshing out slideshow js more

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,7 +3,17 @@
 $(document).ready(function() {
 
 	//get the number of slides we have
-	var slideNumber = $('.slide > section').length;
+	var slideNumber = $('section').length;
+
+	//create an array of our sections
+	var sections = document.getElementsByTagName( "section" );
+	var sectionArray = jquery.makeArray( sections );
+	//count of slides as a check
+	console.log(slideNumber);
+
+
+	//show only the first section initially
+
 
 	/* default setup for the page: one opening screen
 	 * if the right arrow is clicked, we move to the next slide
@@ -11,9 +21,11 @@ $(document).ready(function() {
 
 	 	$(".nextButton").click(function(){
 
-	 		
+	 		//hide the current section
+	 		$('section').hide();
+	 		$(this).next().show();
 
 
 	 	});
 
-} // close the script
+}); // close the script
